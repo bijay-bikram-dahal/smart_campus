5abdb94d012fc228fc89910deb9ed3f3d335a3ae
bijay-bikram-dahal <bijaybikramdahal11@gmail.com>
2026-04-08 10:15:00 +0545

    Stabilize JAX-RS bootstrap and build configuration

--- src/main/java/com/university/smartcampus/SmartCampusApplication.java
+++ src/main/java/com/university/smartcampus/SmartCampusApplication.java
@@ -1,9 +1,21 @@
 package com.university.smartcampus;
 
+import com.university.smartcampus.service.InMemoryStorage;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/api/v1")
-public class SmartCampusApplication extends Application {
-    // Application configuration - automatically scans for @Path annotated classes
+public class SmartCampusApplication extends ResourceConfig {
+
+    public SmartCampusApplication() {
+        packages("com.university.smartcampus");
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bindAsContract(InMemoryStorage.class).in(Singleton.class);
+            }
+        });
+    }
 }
